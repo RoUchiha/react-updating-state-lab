82fd6d9b68985836996d3ff4d71f133bba34ec79
RoUchiha <swaggsikh@gmail.com>
2022-04-20 18:54:30 -0500

    commit tree 017d03052dce9da1b99728002773b1f9e55c4142

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,1 +1,33 @@
 // Code DigitalClicker Component Here
+
+import React from 'react';
+import { DefinePlugin } from 'webpack';
+
+class DigitalClicker extends React.Component {
+
+    constructor() {
+        super();
+        this.state = {
+            timesClicked: 0
+        }
+    }
+
+    handleClick = () => {
+        this.setState((previousState) => {
+            return {
+                timesClicked: previousState + 1
+            }
+        })
+    }
+
+
+    render() {
+        return (
+            <button onClick={this.handleClick}>{this.state.timesClicked}</button>
+        )
+    }
+
+}
+
+
+export default DigitalClicker;
--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -1,1 +1,18 @@
 // Code YouTubeDebugger Component Here
+
+import React from 'react';
+
+class YouTubeDebugger extends React.Component {
+
+
+
+    render() {
+        return (
+            <button></button>
+        )
+
+    }
+}
+
+
+export default YouTubeDebugger;
